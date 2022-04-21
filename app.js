810cd2311a210010bc2e2221b81b5aa19c9b03c6
Celal <celal.alizade.2001@gmail.com>
2022-04-21 18:04:32 +0400

    more changes

--- app.js
+++ app.js
@@ -1,29 +1,47 @@
 let from = 'RUB';
 let to = 'USD';
 
-// Convert from currency (Selection)
+let inputFrom = document.getElementById("from");
+let inputTo = document.getElementById("to");
+
+let labelFrom = document.querySelector(".mini-info-from")
+let labelTo = document.querySelector(".mini-info-to")
+
+
+// Convert from currency to currency (selection and conversion)
 let fromOption = document.querySelectorAll('.currency__from>li');
 fromOption.forEach(button => {
     button.addEventListener('click', function () {
         fromOption.forEach(btn => btn.classList.remove('active'));
         this.classList.add('active');    
-        from = this.textContent;    
+        from = this.textContent;
+        convert(inputFrom,inputTo);
     });
 });
-
-// To currency (Selection)
 let toOption = document.querySelectorAll('.currency__to>li');
 toOption.forEach(button => {
     button.addEventListener('click', function () {
         toOption.forEach(btn => btn.classList.remove('active'));
         this.classList.add('active');
         to = this.textContent;
+        convert(inputFrom,inputTo);
     });
 });
+function convert(f,t){
+    fetch(`https://api.exchangerate.host/latest?base=${from}&symbols=${to}`)
+    .then(res => res.json())
+    .then(data => {
+            t.value = f.value * data.rates[`${to}`];
+            t.value = parseFloat(t.value).toFixed(3);
 
-// Conversion calculation
-let inputFrom = document.getElementById("from");
-let inputTo = document.getElementById("to");
+            // Bottom info currency change
+            labelFrom.textContent = "1 " + from + " = " + Math.round((data.rates[`${to}`]) * 10000) / 10000 + " " + to;
+            labelTo.textContent = "1 " + to + " = " + Math.round((1 / data.rates[`${to}`]) * 10000) / 10000 + " " + from;
+        })
+    offlineMode();
+}
+
+// Conversion calculation on value changed
 inputFrom.addEventListener("input", (e) => {
     fetch(`https://api.exchangerate.host/latest?base=${from}&symbols=${to}`)
     .then(res => res.json())
@@ -32,7 +50,6 @@ inputFrom.addEventListener("input", (e) => {
             inputTo.value = parseFloat(inputTo.value).toFixed(3);
         })
 });
-
 inputTo.addEventListener("input", (e) => {
     fetch(`https://api.exchangerate.host/latest?base=${from}&symbols=${to}`)
     .then(res => res.json())
@@ -42,10 +59,9 @@ inputTo.addEventListener("input", (e) => {
         })
 });
 
-
-
-let test = document.querySelector("#signIn");
-test.addEventListener('click', ()=>{
-    console.log('from: ' + from);
-    console.log("to: " + to);
-})
+function offlineMode(){
+    if(!navigator.onLine){
+        document.querySelector(".converter").style.display = "none";
+        document.querySelector(".offline").style.display = "block";
+    }
+}
